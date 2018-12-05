6fa31aae9542186de68b9f0d411c264ab96f996b
hardy1334 <hardychugh34@gmail.com>
2018-12-05 23:32:54 +0530

    profile setup

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-import { GET_PROFILE, CLEAR_CURRENT_PROFILE, PROFILE_LOADING } from "./types";
+import {
+  GET_PROFILE,
+  CLEAR_CURRENT_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS
+} from "./types";
 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
@@ -16,6 +21,18 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 };
---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,45 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+function InputGroup({ name, placeholder, value, error, icon, onChange }) {
+  return (
+    <div>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text">
+            <i className={icon} />
+          </span>
+        </div>
+        <input
+          className={classnames("form-control form-control-lg", {
+            "is-invalid": error
+          })}
+          placeholder={placeholder}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
+    </div>
+  );
+}
+
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
+
+InputGroup.defaultProps = {
+  type: "text"
+};
+
+export default InputGroup;
