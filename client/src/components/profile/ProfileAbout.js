b3103b57991858a6ea485584d7545f68b819efa3
hardy1334 <hardychugh34@gmail.com>
2018-12-13 23:27:31 +0530

    Show Developers

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,21 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
+    const { profile } = this.props;
+
+    const firstName = profile.user.name.trim().split(" ")[0];
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" />
+        {skill}
+      </div>
+    ));
     return (
-      <div>
-        <h1>PROFILE</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">
+              {firstName.toUpperCase()}'s' Bio
+            </h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>{firstName.toUpperCase()} Doesn't have a bio</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-ProfileAbout.propTypes = {};
 
 export default ProfileAbout;
