c5017a2b68dfc5f79637e274ac71f0c718a763c5
hardy1334 <hardychugh34@gmail.com>
2018-12-19 19:45:04 +0530

    devview

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/posts", posts);
 if (process.env.NODE_env === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
